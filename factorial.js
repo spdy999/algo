d8c13b37564b609e31a72e9c5702227d48244058
spdy999 <1a1a1a.262626.0010@gmail.com>
2021-04-11 14:33:09 +0700

    factorial

---
+++ factorial.js
@@ -0,0 +1,6 @@
+const factorial = (n) => n === 1 ? 1 : n * factorial(n - 1)
+console.log(factorial(3))
+console.log(`this is fac(3): ${factorial(3)}`)
+module.exports = {
+    factorial
+}
---
+++ factorial.test.js
@@ -0,0 +1,9 @@
+const {factorial} = require('./factorial')
+
+test('Should return 6 if n=3', () => {
+    expect(factorial(3)).toBe(6)
+})
+
+test('Should return 24 if n=4', () => {
+    expect(factorial(4)).toBe(24)
+})
